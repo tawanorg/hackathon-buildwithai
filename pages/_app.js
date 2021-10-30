8f2fcaf9ca580351d113aba358038818ba1021dc
Tew Tawan <hello@tawan.org>
2021-10-30 23:41:40 +0700

    fix: changed bg

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import { Box, extendTheme, ChakraProvider } from "@chakra-ui/react"
 
 const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
+  gray: {
+    100: "#fffef8",
   },
 }
 const theme = extendTheme({ colors })
